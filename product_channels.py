6c6cb3229a9f234689cb5d94387af87e32bf8ce6
Pavel Kuznetsov <84262172+JhonnyAxe@users.noreply.github.com>
2025-12-06 13:25:13 +0300

    Update product channels list

--- product_channels.py
+++ product_channels.py
@@ -4,63 +4,73 @@
 
 PRODUCT_CHANNELS = [
     {
-        "name": "Product Jobs | Russia",
-        "members": 0,
+        "name": "job_match_robot",
+        "link": "https://t.me/job_match_robot",
+        "username": "job_match_robot",
+    },
+    {
+        "name": "zarubezhom_jobs",
+        "link": "https://t.me/zarubezhom_jobs",
+        "username": "zarubezhom_jobs",
+    },
+    {
+        "name": "products_jobs",
+        "link": "https://t.me/c/products_jobs/97833",
+        "username": "products_jobs",
+    },
+    {
+        "name": "remote_jobs_relocate",
+        "link": "https://t.me/remote_jobs_relocate",
+        "username": "remote_jobs_relocate",
+    },
+    {
+        "name": "jobforjunior",
+        "link": "https://t.me/jobforjunior",
+        "username": "jobforjunior",
+    },
+    {
+        "name": "remotegeekjob",
+        "link": "https://t.me/remotegeekjob",
+        "username": "remotegeekjob",
+    },
+    {
+        "name": "young_june",
+        "link": "https://t.me/young_june",
+        "username": "young_june",
+    },
+    {
+        "name": "careerstation_pm",
+        "link": "https://t.me/careerstation_pm",
+        "username": "careerstation_pm",
+    },
+    {
+        "name": "products_jobs_projects",
+        "link": "https://t.me/products_jobs_projects",
+        "username": "products_jobs_projects",
+    },
+    {
+        "name": "startup_job_russia",
+        "link": "https://t.me/startup_job_russia",
+        "username": "startup_job_russia",
+    },
+    {
+        "name": "mindset_jobs",
+        "link": "https://t.me/mindset_jobs",
+        "username": "mindset_jobs",
+    },
+    {
+        "name": "product_jobs",
         "link": "https://t.me/product_jobs",
         "username": "product_jobs",
     },
     {
-        "name": "ProductCamp Jobs",
-        "members": 0,
-        "link": "https://t.me/productcamp_jobs",
-        "username": "productcamp_jobs",
+        "name": "hireproproduct",
+        "link": "https://t.me/hireproproduct",
+        "username": "hireproproduct",
     },
     {
-        "name": "Product Sense Community",
-        "members": 0,
-        "link": "https://t.me/productsensejobs",
-        "username": "productsensejobs",
-    },
-    {
-        "name": "Работа: Product / Project",
-        "members": 0,
-        "link": "https://t.me/product_project_jobs",
-        "username": "product_project_jobs",
-    },
-    {
-        "name": "Digital Jobs | Product",
-        "members": 0,
-        "link": "https://t.me/digital_product_jobs",
-        "username": "digital_product_jobs",
-    },
-    {
-        "name": "Practicum Career | Product",
-        "members": 0,
-        "link": "https://t.me/practicum_product_jobs",
-        "username": "practicum_product_jobs",
-    },
-    {
-        "name": "Head of Product | CPO",
-        "members": 0,
-        "link": "https://t.me/cpo_jobs",
-        "username": "cpo_jobs",
-    },
-    {
-        "name": "Product Analyst & Manager",
-        "members": 0,
-        "link": "https://t.me/product_analytics_jobs",
-        "username": "product_analytics_jobs",
-    },
-    {
-        "name": "Remote Product Work",
-        "members": 0,
-        "link": "https://t.me/remote_product_jobs",
-        "username": "remote_product_jobs",
-    },
-    {
-        "name": "Product & UX Jobs",
-        "members": 0,
-        "link": "https://t.me/product_ux_jobs",
-        "username": "product_ux_jobs",
+        "name": "jobstobefound",
+        "link": "https://t.me/jobstobefound",
+        "username": "jobstobefound",
     },
 ]
